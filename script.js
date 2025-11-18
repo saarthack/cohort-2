2aed1ca329ac0ef3fdfaccc5dcb7fc04ae46d595
saarthack <ssarthak2003@outlook.com>
2025-11-18 21:48:08 +0530

    timeout

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
+var grow = 0
 var btn = document.querySelector('button')
-var main = document.querySelector('main')
+var h2 = document.querySelector('h2')
+var inner = document.querySelector('.inner')
 
-var arr = ['Hey! I am Sarthak','Sheryians is Best.','Anubhav is handsome','harsh bhaiya is great.','JS is Love','Keep practicing guys...']
-
-btn.addEventListener('click',function(){    
-
-    var h1 = document.createElement('h1')
+btn.addEventListener('click',function(){
+    btn.style.pointerEvents = 'none'
     
-    var x = Math.random()*80
-    var y = Math.random()*80
-    var rot = Math.random()*360
-    var scl = Math.random()*3
-    var a = Math.floor(Math.random()*arr.length)
-
-    h1.innerHTML = arr[a]
-
-    h1.style.position = 'absolute'
-
-    h1.style.left = x+'%'
-    h1.style.top = y+'%'
-    h1.style.rotate = rot+'deg'
-    h1.style.scale = scl
+    var num = 50 + Math.floor(Math.random()*50)
     
-    main.appendChild(h1)
+    console.log('Your file will be downloaded in',num/10,'seconds');
+
+    var int = setInterval(() => {
+        grow++
+        h2.innerHTML = grow+'%'
+        inner.style.width = grow+'%'
+    },num);
+
+    setTimeout(() => {
+        clearInterval(int)
+        btn.innerHTML = 'Downloaded'
+        btn.style.opacity = 0.5
+    }, num*100);
+
+    
+    
 })
--- task.md
+++ task.md
@@ -1,17 +1,4 @@
-Topics 
+setTimeout - delay
+setInterval - controlled loop
 
-createElement is used to create new element
-appendChild is used to append it in any parent.
-
-What you've to do - 
-1. Make 'main' tag in html, give it height and width 100% 
-2. make a button and center it. (give it position relative and z index)
-3. In JS select button and main div and add a eventlistener on button
-4. Inside a function create new element with the help of document.createElement method. 
-5. If you've successfully created an element print it on console to check it. 
-6. write innerHTML if you want 
-7. Use parent.appendChild method to add this new element in main div.
-
-Task - 
-
-when user clicks on button create h1, put random quote in it (from array), give them random position, rotation, scale, color(optional) and append them to parent
+Task - Make a downloading effect like this. 
